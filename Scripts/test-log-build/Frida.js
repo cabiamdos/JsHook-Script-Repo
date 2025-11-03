5569f1d6aba1428223d98a86d1d9761df85b36d8
William <cabiam@criptext.com>
2025-11-03 01:28:04 +0100

    test-log-build: update the console.log with simple and complex log

--- Scripts/test-log-build/Frida.js
+++ Scripts/test-log-build/Frida.js
@@ -1,7 +1,7 @@
 // 2025/10/04 01:14:09
 // write your script...
 
-Java.perform(function() {
+Java.perform(function () {
   const build = {
     product: Java.use("android.os.Build").PRODUCT.value,
     brand: Java.use("android.os.Build").BRAND.value,
@@ -10,6 +10,22 @@ Java.perform(function() {
     hardware: Java.use("android.os.Build").HARDWARE.value,
     fingerprint: Java.use("android.os.Build").FINGERPRINT.value
   }
+  console.log('[+] printing simple build object')
+  console.log(JSON.stringify(build, null, 2));
+  console.log('\n')
 
-  console.log(JSON.stringify(build,null, 2))
+  var Build = Java.use("android.os.Build");
+  var fields = Build.class.getDeclaredFields();
+  console.log('[+] printing complex build object');
+  console.log(fields)
+  fields.forEach(function (field) {
+    field.setAccessible(true);
+    try {
+      var value = field.get(null); // static field: null instance
+      console.log(field.getName() + ": " + value);
+    } catch (e) {
+      console.log(field.getName() + ": <unable to access>");
+    }
+  });
+  
 })
